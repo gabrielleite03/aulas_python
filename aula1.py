112793d1f5786af91976d2f3c442c2cc4aa10497
Gabriel Leite <gabriel.leite@mercadolivre.com>
2024-03-14 11:59:07 -0300

    First commit

---
+++ aula1.py
@@ -0,0 +1,35 @@
+print('Hello, world')
+
+# comentário
+"""
+Estou adorando Python
+"""
+print('Outra coisa')
+print(1, 34)
+print( 1,34, sep="-")
+print( 1,34, sep="-", end="\n")
+
+
+
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
---
+++ aula22.py
@@ -0,0 +1,55 @@
+#Operador Lógico "or"
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# or - Qualquer condição verdadeira avalia
+# toda a expressão como verdadeira.
+# Se qualquer valor for considerado verdadeiro,
+# a expressão inteira será avaliada naquele valor.
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
+
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+# senha = input('Senha: ')
+print(not True)  # False
+print(not False)  # True
+
+# Operadores in e not in
+# Strings são iteráveis
+#  0 1 2 3 4 5
+#  O t á v i o
+# -6-5-4-3-2-1
+# nome = 'Otávio'
+# print(nome[2])
+# print(nome[-4])
+# print('vio' in nome)
+# print('zero' in nome)
+# print(10 * '-')
+# print('vio' not in nome)
+# print('zero' not in nome)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
